750d79166a44f70ad5371ed874c77e3708137594
VinceDeslo <v.desloover2@gmail.com>
2021-01-11 15:19:36 -0500

    Cleanup and branding

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Button } from "@material-ui/core"
 
 function App() {
   return (
     <div className="App">
-      <h1>Howdy</h1>
+      <Button variant="outlined">Howdy</Button>
     </div>
   );
 }
