319464ee590cb53027d19b8eaa95dbacdb6c01f8
VinceDeslo <v.desloover2@gmail.com>
2021-01-12 22:01:58 -0500

    Opening and closing of sidebar functional

--- frontend-service/src/components/Sidebar.tsx
+++ frontend-service/src/components/Sidebar.tsx
@@ -1,40 +1,62 @@
 import React from 'react';
-//import clsx from 'clsx';
+import clsx from 'clsx';
 import { Drawer, Divider, List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
-import { MdNavigateNext, MdSettings, MdFolderShared, MdStar } from "react-icons/md";
+import { MdNavigateBefore, MdNavigateNext, MdSettings, MdFolderShared, MdStar } from "react-icons/md";
 import { useSidebarStyles } from '../styles/sidebar-styles';
 
 function Sidebar() {
 
+    // Import styling
     const style = useSidebarStyles();
 
+    // Drawer state
+    const [open, setOpen] = React.useState(false);
+
+    // Hook for handling drawer movement
+    const toggleDrawer = () => {
+        if(open) setOpen(false);
+        else setOpen(true)
+    }
+
+    // Sidebar component tree
     return(
         <div className={style.root}>
             <Drawer
-            className={style.drawer} 
-            variant="permanent">
-            <div className={style.drawerContent}>
-            <List>
-                <ListItem button>
-                    <ListItemIcon><MdNavigateNext/></ListItemIcon>
-                </ListItem>
-                <Divider />
-                <ListItem button>
-                    <ListItemIcon><MdFolderShared/></ListItemIcon>
-                    <ListItemText>Applications</ListItemText> 
-                </ListItem>
-                <ListItem button>
-                    <ListItemIcon><MdStar/></ListItemIcon>
-                    <ListItemText>Favorites</ListItemText> 
-                </ListItem>
-                <ListItem button>
-                    <ListItemIcon><MdSettings/></ListItemIcon>
-                    <ListItemText>Settings</ListItemText> 
-                </ListItem>
-            </List>
-        </div>
-        </Drawer>
-
+            className={clsx(style.drawer, {
+                [style.drawerOpen]: open,
+                [style.drawerClose]: !open,
+            })}
+            classes={{
+                paper: clsx({
+                  [style.drawerOpen]: open,
+                  [style.drawerClose]: !open,
+                }),
+            }} 
+            variant="permanent"
+            >
+                <div className={style.drawerContent}>
+                    <List>
+                        <ListItem button>
+                            <ListItemIcon onClick={toggleDrawer}>
+                                {open? <MdNavigateBefore/> : <MdNavigateNext/>}
+                            </ListItemIcon>
+                        </ListItem>
+                        <Divider />
+                        <ListItem button>
+                            <ListItemIcon><MdFolderShared/></ListItemIcon>
+                            <ListItemText>Applications</ListItemText> 
+                        </ListItem>
+                        <ListItem button>
+                            <ListItemIcon><MdStar/></ListItemIcon>
+                            <ListItemText>Favorites</ListItemText> 
+                        </ListItem>
+                        <ListItem button>
+                            <ListItemIcon><MdSettings/></ListItemIcon>
+                            <ListItemText>Settings</ListItemText> 
+                        </ListItem>
+                    </List>
+                </div>
+            </Drawer>
         </div>
     );
 }
--- frontend-service/src/styles/sidebar-styles.ts
+++ frontend-service/src/styles/sidebar-styles.ts
@@ -8,10 +8,28 @@ export const useSidebarStyles = makeStyles((theme:Theme) => ({
     drawer: {
         width: 240,
         flexShrink: 0,
+        whiteSpace: 'nowrap',
     },
     drawerContent: {
         paddingTop: 60,
-        overflow: 'auto',
-    }
+    },
+    drawerOpen: {
+        width: 240,
+        transition: theme.transitions.create('width', {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    drawerClose: {
+        transition: theme.transitions.create('width', {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.leavingScreen,
+        }),
+        overflowX: 'hidden',
+        width: theme.spacing(7) + 1,
+        [theme.breakpoints.up('sm')]: {
+          width: theme.spacing(6) + 1,
+        },
+      },
     
 }));
