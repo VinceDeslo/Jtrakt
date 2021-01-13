2766ed64ce3f6cba0aba83922e1d288dddda0c23
VinceDeslo <v.desloover2@gmail.com>
2021-01-13 09:13:26 -0500

    Router and content components added

--- frontend-service/src/App.tsx
+++ frontend-service/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Button } from "@material-ui/core";
+import { Button } from '@material-ui/core';
 import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar'
+import Sidebar from './components/Sidebar';
+import { Route, NavLink, HashRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Sidebar/>
+      <Button>content</Button>
     </div>
   );
 }
---
+++ frontend-service/src/components/PageContent.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function PageContent() {
+    return(
+        <h1>PageContent</h1>
+    );
+}
+
+export default PageContent;
